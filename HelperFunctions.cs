749ee2b0c162d70a70c47693d7bc4945da18216f
balogun14 <awwalbalogun06@gmail.com>
2023-11-23 05:37:02 +0100

    Add exit

--- HelperFunctions.cs
+++ HelperFunctions.cs
@@ -32,51 +32,57 @@ namespace ConsoleBankApp
             1. Deposit Funds.
             2. Withdrawl Funds.
             3. Check Balance
+            4. Exit
             ";
             Console.WriteLine(operationYoucanPerform);
         }
-           }
     }
-    
-    class InputFunctions<T>
-    {
-        public static T UserInput(string message)
-        {
-            Console.Write(message + ": ");
-            string value = Console.ReadLine()!;
-            T output = (T)Convert.ChangeType(value, typeof(T));
-            return output;
+}
 
-        }
+class InputFunctions<T>
+{
+    public static T UserInput(string message)
+    {
+        Console.Write(message + ": ");
+        string value = Console.ReadLine()!;
+        T output = (T)Convert.ChangeType(value, typeof(T));
+        return output;
+
     }
-    
-    class OperationFunctions<T> where T : CustomerAccount
-    {
-        public static void AccountOperations(T account)
-        {
-            bool condition = true;
+}
 
-            while (condition)
+class OperationFunctions<T> where T : CustomerAccount
+{
+    public static void AccountOperations(T account)
+    {
+        bool condition = true;
+
+        while (condition)
+        {
+            HelperFunctions.DashBoard();
+            int choice = int.Parse(Console.ReadLine()!);
+            switch (choice)
             {
-                HelperFunctions.DashBoard();
-                int choice = int.Parse(Console.ReadLine()!);
-                switch (choice)
-                {
-                    case 1:
-                        account.PayInFunds();
-                        break;
-                    case 2:
-                        Console.Write("How much are you withdrawing: ");
-                        decimal amount = decimal.Parse(Console.ReadLine()!);
-                        account.WithdrawFunds(amount);
-                        break;
-                    case 3:
-                        account.DisplayDetails();
-                        break;
-                    default:
-                        HelperFunctions.MessageWithColor("Option Not Available", ConsoleColor.DarkRed);
-                        break;
-                }
+                case 1:
+                    account.PayInFunds();
+                    break;
+                case 2:
+                    Console.Write("How much are you withdrawing: ");
+                    decimal amount = decimal.Parse(Console.ReadLine()!);
+                    account.WithdrawFunds(amount);
+                    break;
+                case 3:
+                    account.DisplayDetails();
+                    break;
+                case 4:
+                    HelperFunctions.MessageWithColor("You exited Succesfully");
+                    condition = false;
+                    break;
+                default:
+                    HelperFunctions.MessageWithColor("Option Not Available", ConsoleColor.DarkRed);
+                    break;
             }
+            Console.Clear();
         }
     }
+}
